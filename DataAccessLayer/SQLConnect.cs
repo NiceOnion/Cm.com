25555cb65fcb887121b06454e3bf46873754d5dc
Stijn Versluis <stijn.versluis@gmail.com>
2023-03-16 10:39:40 +0100

    Add Layers and database connection

---
+++ DataAccessLayer/SQLConnect.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    public class SQLConnect
+    {
+        string dbName = "dbi482774_dfeditor";
+        string dbPass = "DFEditor0123";
+        internal DbConnection DbConnection; 
+
+        internal void InitializeDB()
+        {
+            string connectionString = $"Data Source=mssqlstud.fhict.local;Database={dbName};User Id={dbName};Password={dbPass};MultipleActiveResultSets=true;";
+            DbConnection = new SqlConnection(connectionString);
+        }
+
+        internal bool OpenConnection()
+        {
+            try
+            {
+                DbConnection.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        internal bool CloseConnection()
+        {
+            try
+            {
+                DbConnection.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
