536eb9db6b463ca58911cc1ae34b72b975638d6a
Amin El Abdellaoui <i467072@fhict.nl>
2023-03-16 14:40:27 +0100

    Functions added

---
+++ BusinessLayer/Account/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceLayer;
+
+namespace BusinessLayer
+{
+    public class Account
+    {
+        public int ID { get; private set; }
+        public string Name { get; private set; }
+        public string Password { get; private set; }
+
+        public Account(int id, string name, string password)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Password = password;
+        }
+
+        public Account(AccountDTO accountDTO)
+        {
+            this.ID = accountDTO.ID;
+            this.Name = accountDTO.Name;
+            this.Password = accountDTO.Password;
+        }
+
+        public AccountDTO ToAccountDTO()
+        {
+            return new(this.ID, this.Name, this.Password);
+        }
+    }
+}
