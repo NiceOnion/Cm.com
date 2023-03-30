9cccef239b615002592bb751c1032a3a9dcddfb9
Amin El Abdellaoui <i467072@fhict.nl>
2023-03-30 10:13:39 +0200

    fixed untracked files

---
+++ DemoFlowEditor/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoFlowEditor.Controllers
+{
+    [ApiController]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        [Route("api/get")]
+        public IActionResult Index()
+        {
+            var result = "bro";
+            return new JsonResult(result);
+        }
+    }
+}
